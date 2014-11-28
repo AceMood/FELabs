deaa9b00c55d74670f45a12fdde2cb64ac0eb319
AceMood <zmike86@163.com>
2014-11-28 21:06:02 +0800

    add comments

--- MDN/Using Files From Web Application/index.php
+++ MDN/Using Files From Web Application/index.php
@@ -1,6 +1,8 @@
 <?php
 if (isset($_FILES['myFile'])) {
     // Example:
+    // (PHP 4 >= 4.0.3, PHP 5) 支持move_uploaded_file方法,
+    // 参见http://php.net/manual/zh/function.move-uploaded-file.php
     move_uploaded_file($_FILES['myFile']['tmp_name'], "uploads/" . $_FILES['myFile']['name']);
     exit;
 }
