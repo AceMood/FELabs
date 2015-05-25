798c0613a03e7cadebc3ec64a1bc6668a1e43a7e
AceMood <zmike86@163.com>
2015-05-25 23:56:26 +0800

    dev

--- Do/npm-glob/pattern.js
+++ Do/npm-glob/pattern.js
@@ -10,22 +10,13 @@ var files;
 files = glob.sync('s*(t|a|b)ar.js', options);
 console.log('s*(t|a|b)ar.js match:     ' + files);
 
+files = glob.sync('s?(t|a|b)ar.js', options);
+console.log('s?(t|a|b)ar.js match:     ' + files);
+
 // 匹配 1 或 多个模式中的字符
 files = glob.sync('s+(t|a|b)ar.js', options);
 console.log('s+(t|a|b)ar.js match:     ' + files);
 
-// 如果**作为路径名的一部分
-files = glob.sync('dir*/*.js', options);
-console.log('dir*/*.js match:          ' + files);
-
-// dot作为特殊字符
-files = glob.sync('*.*', options);
-console.log('*.* match:      ' + files);
-
-// 匹配当前目录全部文件和目录
-files = glob.sync('*', options);
-console.log('* match:        ' + files);
-
-// *出现在文件名中匹配 0 或 多个字符
-files = glob.sync('in*', options);
-console.log('in* match:      ' + files);
+// 匹配模式中确认出现一项
+files = glob.sync('s@(t|a|b)ar.js', options);
+console.log('s@(t|a|b)ar.js match:     ' + files);
