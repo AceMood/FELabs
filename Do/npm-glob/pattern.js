46684938fbd639e6907b9fcd634cabb75484230e
AceMood <zmike86@163.com>
2015-05-25 23:14:06 +0800

    dev

--- Do/npm-glob/pattern.js
+++ Do/npm-glob/pattern.js
@@ -6,18 +6,17 @@ var options = {
 };
 var files;
 
-// 默认只匹配当前目录js文件
-files = glob.sync('*.js', options);
-console.log('*.js match:     ' + files);
+// 匹配 0 或 多个模式中的字符
+files = glob.sync('s*(t|a|b)ar.js', options);
+console.log('s*(t|a|b)ar.js match:     ' + files);
 
-// 匹配一级目录下的js文件
-files = glob.sync('*/*.js', options);
-console.log('*/*.js match:   ' + files);
+// 匹配 1 或 多个模式中的字符
+files = glob.sync('s+(t|a|b)ar.js', options);
+console.log('s+(t|a|b)ar.js match:     ' + files);
 
-// 如果**单独作为路径的一部分, 则
-// 匹配当前目录或多极子目录下的js文件
-files = glob.sync('**/*.js', options);
-console.log('**/*.js match:  ' + files);
+// 如果**作为路径名的一部分
+files = glob.sync('dir*/*.js', options);
+console.log('dir*/*.js match:          ' + files);
 
 // dot作为特殊字符
 files = glob.sync('*.*', options);
