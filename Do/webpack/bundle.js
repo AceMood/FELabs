f878d0be0a3037feafd14a54759aa44b0dbfe1d8
AceMood <zmike86@163.com>
2015-08-09 01:19:59 +0800

    add

--- Do/webpack/bundle.js
+++ Do/webpack/bundle.js
@@ -44,20 +44,26 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	__webpack_require__(1);
-	document.write(__webpack_require__(5));
+	document.write(__webpack_require__(1));
+	__webpack_require__(2);
 
 /***/ },
 /* 1 */
+/***/ function(module, exports) {
+
+	module.exports = "It works from content.js.";
+
+/***/ },
+/* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
 	// style-loader: Adds some css to the DOM by adding a <style> tag
 
 	// load the styles
-	var content = __webpack_require__(2);
+	var content = __webpack_require__(3);
 	if(typeof content === 'string') content = [[module.id, content, '']];
 	// add the styles to the DOM
-	var update = __webpack_require__(4)(content, {});
+	var update = __webpack_require__(5)(content, {});
 	if(content.locals) module.exports = content.locals;
 	// Hot Module Replacement
 	if(false) {
@@ -74,10 +80,10 @@
 	}
 
 /***/ },
-/* 2 */
+/* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
-	exports = module.exports = __webpack_require__(3)();
+	exports = module.exports = __webpack_require__(4)();
 	// imports
 
 
@@ -88,7 +94,7 @@
 
 
 /***/ },
-/* 3 */
+/* 4 */
 /***/ function(module, exports) {
 
 	/*
@@ -144,7 +150,7 @@
 
 
 /***/ },
-/* 4 */
+/* 5 */
 /***/ function(module, exports, __webpack_require__) {
 
 	/*
@@ -367,12 +373,6 @@
 			URL.revokeObjectURL(oldSrc);
 	}
 
-
-/***/ },
-/* 5 */
-/***/ function(module, exports) {
-
-	module.exports = "It works from content.js.";
 
 /***/ }
 /******/ ]);
