6c9a20dbf5243f3e56c7ebc0003f253faf43249a
AceMood <zmike86@163.com>
2015-08-16 13:59:08 +0800

    add htmlparser2 demo

---
+++ Do/htmlparser2/index.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+var htmlparser = require("htmlparser2");
+
+
+var parser = new htmlparser.Parser({
+  onopentag: function (name, attribs) {
+    debugger;
+    if(name === "script" && attribs.type === "text/javascript"){
+      console.log("JS! Hooray!");
+    }
+  },
+  ontext: function (text) {
+    debugger;
+    console.log("-->", text);
+  },
+  onclosetag: function (tagname) {
+    debugger;
+    if (tagname === "script") {
+      console.log("That's it?!");
+    }
+  }
+}, {
+  decodeEntities: true
+});
+
+
+var html = fs.readFileSync('./test.html');
+parser.write(html);
+parser.end();
+
+
