dc7d962471cffcce09c37c762e65324db567b71c
AceMood <zmike86@163.com>
2015-08-19 23:13:14 +0800

    add html-tokenize

--- Do/html-tokenize/index.js
+++ Do/html-tokenize/index.js
@@ -1,3 +1,14 @@
-/**
- * Created by acemood on 15-8-16.
- */
+var fs = require('fs');
+var tokenize = require('html-tokenize');
+var through = require('through2');
+
+fs.createReadStream(__dirname + '/test.html')
+    .pipe(tokenize())
+    .pipe(through.obj(function (row, enc, next) {
+
+      debugger;
+
+      row[1] = row[1].toString();
+      console.log(row);
+      next();
+    }));
