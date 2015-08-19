68be64f771da6c7b704f740d40eee1f799b41d23
AceMood <zmike86@163.com>
2015-08-19 23:23:20 +0800

    test lessjs

--- Do/lessjs/index.js
+++ Do/lessjs/index.js
@@ -2,11 +2,21 @@ var less = require('less');
 var fs = require('fs');
 
 
-var cssText = fs.readFileSync('./style.less', {
+var cssText_0 = fs.readFileSync('./style.less', {
   encoding: 'utf8'
 });
 
+var cssText_1 = fs.readFileSync('./other.less', {
+  encoding: 'utf8'
+});
+
+
+var cssText = cssText_0 + '\n' + cssText_1;
+
+fs.writeFileSync('full.less', cssText);
+
 less.render(cssText, function (e, output) {
   // console.log(output.css);
+  debugger;
   fs.writeFileSync('output.css', output.css);
 });
